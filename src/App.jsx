1cc846b6b0a0e63acbc0027fa8bed248d76dafc4
frank <frankmolinaj9604@hotmail.com>
2022-05-30 13:07:23 -0300

    4ta subida

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import { Spinner } from "react-bootstrap";
 import './style/app.css'
 
 function App() {
-  const correct=new Audio(audio)
+  const correct = new Audio(audio)
   const [paises, setPaises] = useState([])
   const [num, setnum] = useState(0)
   const [value, setvalue] = useState('')
@@ -36,7 +36,7 @@ function App() {
     correct.play()
     let a = getRandomInt(0, 249)
     setnum(a)
-    setPuntaje(Puntaje+1)
+    setPuntaje(Puntaje + 1)
     procesarNombre(paises[a].translations.es)
   }
   const update = (e) => {
@@ -53,17 +53,17 @@ function App() {
     if (procesar(value) === procesar(paises[num].translations.es)) {
       setvalue('')
       NewPais()
-    } 
+    }
   }
   const remAcent = (str) => {
-    const acentos = { 'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U',',':'','.':'' };
+    const acentos = { 'á': 'a', 'é': 'e', 'í': 'i', 'ó': 'o', 'ú': 'u', 'Á': 'A', 'É': 'E', 'Í': 'I', 'Ó': 'O', 'Ú': 'U', ',': '', '.': '' };
     return str.split('').map(letra => acentos[letra] || letra).join('').toString();
   }
   function procesar(str) {
     return remAcent(str.toUpperCase().trim())
   }
   function procesarNombre(str) {
-    
+
     let a = str.length
     let newstr = ''
     for (let i = 0; i < a; i++) {
@@ -84,7 +84,7 @@ function App() {
     setNombrePais(procesar(newstr))
   }
   function resolverNombre() {
-    Puntaje===0?setPuntaje(0):setPuntaje(Puntaje-1)
+    Puntaje === 0 ? setPuntaje(0) : setPuntaje(Puntaje - 1)
     let str = NombrePais
     let a = str.length
     let nombreOg = paises[num].translations.es
@@ -118,7 +118,7 @@ function App() {
       {paises[0] ?
         !NombrePais ?
           <div className=" d-flex flex-column justify-content-center">
-            <h1 className="m-5 mt-0-sm mt-5">La mejor trivia sobre paises</h1>
+            <h1 className="m-5 mt-0-sm mt-5 text-center">La mejor trivia sobre paises</h1>
             <div className="d-flex justify-content-center">
               <button className="mt-5  px-5 btn btn-warning" onClick={empezar}>Empezar</button>
             </div>
@@ -126,7 +126,7 @@ function App() {
           :
           <div className="card mt-3 mt-0-sm border-light shadow-sm" style={{ width: "18rem" }}>
             <p className="p-1 pe-2 puntaje position-absolute top-0 start-0 bg-light fw-bold">Puntaje:{Puntaje}</p>
-              <img src={foco} onClick={resolverNombre} className="m-1 position-absolute top-0 end-0 bombilla" />
+            <img src={foco} onClick={resolverNombre} className="m-1 position-absolute top-0 end-0 bombilla" />
             <img src={paises[num].flags.png} height={"160"} className="card-img-top" alt="Bandera" />
             <div className="card-body">
               <h5 className="card-title letter-spacing text-center mb-3">{NombrePais}</h5>
@@ -141,7 +141,7 @@ function App() {
             </div>
           </div>
         :
-        <div className="d-flex align-items-end">
+        <div className="d-flex align-items-end mt-5 mt-0-sm">
           <h5 className="m-0 me-2 align-self-center">Cargando</h5>
           <Spinner animation="border" role="status">
             <span className="visually-hidden">Loading...</span>
